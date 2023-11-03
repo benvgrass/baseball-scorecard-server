8435a92dc19af96053c86edab723818c862f2823
Ben Grass <bvgrass@outlook.com>
2023-11-03 15:15:57 -0400

    Remove unused imports

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -19,7 +19,7 @@ pub struct Response {
 type Result<T, E = Debug<mongodb::error::Error>> = std::result::Result<T, E>;
 
 #[get("/")]
-pub async fn index(mut db: Connection<MongoClient>) -> Result<Json<Response>> {
+pub async fn index(db: Connection<MongoClient>) -> Result<Json<Response>> {
     let client: Client = db.into_inner();
     let database: Database = client.database("GameData");
     let coll: Collection<Response> = database.collection("Games");
