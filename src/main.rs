0943a22620f6567091e706dc874b36c253876211
Ben Grass <bvgrass@outlook.com>
2023-11-03 12:42:39 -0400

    Add test for index route and update index function to query MongoDB

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,17 @@ fn rocket() -> _ {
     .attach(db::MongoClient::init())
     .mount("/", routes![routes::index])
 }
+
+#[cfg(test)]
+mod tests {
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+    use rocket::serde::json::Json;
+    use super::rocket;
+
+    #[test]
+    fn test_index() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);    }
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,33 @@
+use rocket::futures::TryFutureExt;
+use rocket::serde::json::Json;
+use rocket_db_pools::Connection;
+use rocket_db_pools::{mongodb::bson::doc, mongodb};
+use rocket_db_pools::mongodb::{Client, Collection, Database};
+use serde::{Serialize, Deserialize};
+use crate::db::MongoClient;
+use rocket::response::Debug;
+// use rocket_db_pools::mongodb::bson::oid::ObjectId;
+
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Response {
+    #[serde(skip_deserializing, skip_serializing_if = "Option::is_none")]
+    _id: Option<i64>,
+    name: String,
+    short_name: String,
+}
+
+type Result<T, E = Debug<mongodb::error::Error>> = std::result::Result<T, E>;
 
 #[get("/")]
-pub fn index() -> &'static str {
-    "Hello, world!"
+pub async fn index(mut db: Connection<MongoClient>) -> Result<Json<Response>> {
+    let client: Client = db.into_inner();
+    let database: Database = client.database("GameData");
+    let coll: Collection<Response> = database.collection("Games");
+    let result = coll.find_one(doc! {"short_name": "hello"}, None)
+        .map_ok( |r|
+            Json( r.unwrap())).await?;
+
+    println!("{:?}", result);
+    Ok(result)
 }
