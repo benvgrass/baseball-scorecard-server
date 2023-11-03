60d28ae3287afa9dcfe1b93bfe7ca5b4400a3497
Ben Grass <bvgrass@outlook.com>
2023-11-03 16:24:00 -0400

    Add test case for index route that checks response body

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,6 +2,9 @@ use crate::db::MongoClient;
 use rocket::futures::TryFutureExt;
 use rocket::response::Debug;
 use rocket::serde::json::Json;
+use rocket_db_pools::Connection;
+use rocket_db_pools::mongodb::bson::oid::ObjectId;
+use rocket_db_pools::{mongodb::bson::doc, mongodb};
 use rocket_db_pools::mongodb::{Client, Collection, Database};
 use rocket_db_pools::Connection;
 use rocket_db_pools::{mongodb, mongodb::bson::doc};
@@ -9,25 +12,22 @@ use serde::{Deserialize, Serialize};
 // use rocket_db_pools::mongodb::bson::oid::ObjectId;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Response {
-    #[serde(skip_deserializing, skip_serializing_if = "Option::is_none")]
-    _id: Option<i64>,
-    name: String,
-    short_name: String,
+pub struct ResponseDocument {
+    pub _id: ObjectId,
+    pub name: String,
+    pub short_name: String,
 }
 
 type Result<T, E = Debug<mongodb::error::Error>> = std::result::Result<T, E>;
 
 #[get("/")]
-pub async fn index(db: Connection<MongoClient>) -> Result<Json<Response>> {
+pub async fn index(db: Connection<MongoClient>) -> Result<Json<ResponseDocument>> {
     let client: Client = db.into_inner();
     let database: Database = client.database("GameData");
-    let coll: Collection<Response> = database.collection("Games");
-    let result = coll
-        .find_one(doc! {"short_name": "hello"}, None)
-        .map_ok(|r| Json(r.unwrap()))
-        .await?;
-
-    println!("{:?}", result);
+    let coll: Collection<ResponseDocument> = database.collection("Games");
+    let result: Json<ResponseDocument> = coll.find_one(doc! {"short_name": "hello"}, None)
+        .map_ok(|r| {
+            Json(r.unwrap())
+        }).await?;
     Ok(result)
 }
