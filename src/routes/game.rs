9188187e86aab286a4ddc329e08d6e43e94db1d3
Ben Grass <bvgrass@outlook.com>
2023-11-03 23:53:49 -0400

    Add route to insert game into database

---
+++ src/routes/game.rs
@@ -0,0 +1,13 @@
+use rocket::{http::Status, serde::json::Json};
+use rocket_db_pools::Connection;
+use crate::{db::{game, MongoClient}, models::game::{Game, InsertGameResponse}};
+
+
+#[post("/game", data = "<game>")]
+pub async fn insert_game(db: Connection<MongoClient>, game: Json<Game>) -> (Status, Option<Json<InsertGameResponse>>) {
+    if let Ok(game_response) = game::insert_game(db, game.into_inner()).await {
+        (Status::Created, Some(Json(game_response)))
+    } else {
+        (Status::ServiceUnavailable, None)
+    }
+}
